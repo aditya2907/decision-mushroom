16ec5d7584708a34a5bd22d145af2ee7e2e18457
heikalb <heikal93@gmail.com>
2019-11-12 21:25:02 -0800

    done with scikit part

--- decision_tree_scikit.py
+++ decision_tree_scikit.py
@@ -1,1 +1,36 @@
-import sklearn
+import get_mushroom_data
+from sklearn.tree import DecisionTreeClassifier
+
+
+def main():
+    # Get mushroom data
+    features, labels = get_mushroom_data.get_data()
+
+    # Split data into training and test sets
+    split = int(len(features)*0.8)
+    train_features = features[:split]
+    train_labels = labels[:split]
+    test_features = features[split:]
+    test_labels = labels[split:]
+
+    # Train decision tree on training data
+    decision_tree = DecisionTreeClassifier()
+    decision_tree = decision_tree.fit(train_features, train_labels)
+
+    # Test decision tree on test data
+    # Iterative testing
+    for i in range(len(test_features)):
+        print(f'Features: {test_features[i]}    '
+              f'Actual label: {test_labels[i]}  '
+              f'Predicted label: {decision_tree.predict([test_features[i]])}')
+
+    # Show accuracy over test set
+    accuracy = decision_tree.score(test_features, test_labels)
+    print(f'Accuracy: {accuracy}')
+
+
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
--- get_mushroom_data.py
+++ get_mushroom_data.py
@@ -1,6 +1,6 @@
 """
 Get and process dataset on mushrooms consisting of labels ('p (poisonous) vs.
-e(edible)), and 22 nominal attributes. Fix missing values based on plurality.
+e(edible)), and 22 nominal features. Fix missing values based on plurality.
 Heikal Badrulhisham, 2019 <heikal93@gmail.com>
 """
 
@@ -9,21 +9,21 @@ from collections import Counter
 
 def missing_values(example, funct):
     """
-    Tell what attributes are missing a value for an example
-    :param example: a list of attribute values of an example
+    Tell what features are missing a value for an example
+    :param example: a list of feature values of an example
     :param funct: function for determining what counts as a missing value
-    :return: indices of attributes with a missing value
+    :return: indices of features with a missing value
     """
     return [i for i in range(len(example)) if funct(example[i])]
 
 
 def fix_missing_values(examples):
     """
-    Fix missing attribute values by filling in with the value that is most
-    common for the attribute.
-    :param examples: list of examples (labels + attributes)
+    Fix missing feature values by filling in with the value that is most
+    common for the feature.
+    :param examples: list of examples (labels + features)
     """
-    # Get the attribute values that are most common in each column
+    # Get the feature values that are most common in each column
     column_pluralities = dict()
 
     for col in range(len(examples[0])):
@@ -31,7 +31,7 @@ def fix_missing_values(examples):
         column_counter = Counter(column_values)
         column_pluralities[col] = column_counter.most_common()[0]
 
-    # Fill in missing attributes with the most common value in the column
+    # Fill in missing features with the most common value in the column
     for ex in examples:
         missing_vals = missing_values(ex, lambda x: '?' in x)
 
@@ -39,10 +39,25 @@ def fix_missing_values(examples):
             ex[i] = column_pluralities[i][0]
 
 
+def convert_data(examples):
+    """
+    Convert string labels and feature values of examples to integers.
+    :param examples: list of labels and feature values
+    """
+    # Convert column by column
+    for col in range(len(examples[0])):
+        # Assign each value of an feature to an integer
+        value_set = list(set([row[col] for row in examples]))
+
+        # Replace string data in the column with integers
+        for row in examples:
+            row[col] = value_set.index(row[col])
+
+
 def get_data():
     """
     Get and process mushroom dataset, fix missing values
-    :return: a list of mushroom attributes and a list of mushroom labels
+    :return: a list of mushroom features and a list of mushroom labels
     """
     # Get data from file
     with open('agaricus-lepiota.data', 'r') as f:
@@ -52,8 +67,12 @@ def get_data():
     examples = [l.split(',') for l in file_lines if l]
     # Fill in missing values
     fix_missing_values(examples)
-    # Separate attributes and labels
-    attributes = [e[1:] for e in examples]
+    # Convert values to integers
+    convert_data(examples)
+    # Separate features and labels
+    features = [e[1:] for e in examples]
     labels = [e[0] for e in examples]
 
-    return attributes, labels
+    return features, labels
+
+get_data()

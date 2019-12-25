6ea7a6bcf8ac2f4fb7a22c71ce25dda572b86406
heikalb <heikal93@gmail.com>
2019-12-24 22:42:12 -0800

    finished, +documentation

--- classify_nn.py
+++ classify_nn.py
@@ -1,29 +1,59 @@
+"""
+Predict whether a mushroom is poisonous or edible using a neural network.
+Heikal Badrulhisham, 2019 <heikal93@gmail.com>
+"""
 import tensorflow as tf
 import pandas
 
 
-def main():
+def get_datasets():
+    """
+    Get mushroom data and prepare them for use in a Tensorflow model
+    :return: training dataset and testing dataset
+    """
+    # Get data
     dataframe = pandas.read_csv('agaricus-lepiota.data')
 
+    # Convert data type for all variables
     for column in dataframe:
         dataframe[column] = pandas.Categorical(dataframe[column])
         dataframe[column] = dataframe[column].cat.codes
 
+    # Get labels
     target = dataframe.pop('p')
 
-    split = int(len(dataframe)*0.8)
-    train_dataset = tf.data.Dataset.from_tensor_slices((dataframe[:split].values, target[:split].values))
+    # Get tensors, and split data into training and test sets
+    split = int(len(dataframe) * 0.8)
+    train_dataset = tf.data.Dataset.from_tensor_slices(
+        (dataframe[:split].values, target[:split].values))
+
     train_dataset = train_dataset.shuffle(len(dataframe)).batch(1)
 
-    test_dataset = tf.data.Dataset.from_tensor_slices((dataframe[split:].values, target[split:].values))
+    test_dataset = tf.data.Dataset.from_tensor_slices(
+        (dataframe[split:].values, target[split:].values))
+
     test_dataset = test_dataset.shuffle(len(dataframe)).batch(1)
 
-    layers = [tf.keras.layers.Dense(10, activation='sigmoid'),
-              tf.keras.layers.Dense(10, activation='sigmoid'),
+    return train_dataset, test_dataset
+
+
+def main():
+    """
+    Get datasets, build and train a neural network model and test said model
+    on unseen data.
+    """
+    # Get datasets
+    train_dataset, test_dataset = get_datasets()
+
+    # Build neural network
+    layers = [tf.keras.layers.Dense(22, activation='sigmoid'),
+              tf.keras.layers.Dense(30, activation='sigmoid'),
               tf.keras.layers.Dense(1, activation='sigmoid')]
 
     model = tf.keras.models.Sequential(layers)
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
     model.fit(train_dataset, epochs=10)
 
     # Test model
